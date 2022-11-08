28d953c1e6512a45708bfda02e12c5dc5d068be4
Alejandro Guzman <ketokunxd@gmail.com>
2022-11-08 00:04:55 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Alejandro Guzman Bolivar on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
